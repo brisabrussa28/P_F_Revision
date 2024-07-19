6b92f1b25888690c4c5bd75e4f795952977b9693
Brisa Aylen Brussa <bbrussa@frba.utn.edu.ar>
2024-07-19 18:48:53 -0300

    organizando

--- src/Library.hs
+++ src/Library.hs
@@ -2,21 +2,22 @@ module Library where
 import PdePreludat
 import Data.Char (toLower)
 
----Autores y Obras 
-
---Punto 1 (ok)
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+{----------------------------------------------Autores y Obras-----------------------------------------------} 
+-------------------------------------------------Punto 1 (ok)-------------------------------------------------
+{------------------------------------------------------------------------------------------------------------}
 --Modelo de Autor
 data Autor = UnAutor{
     nombre_autor :: String,
     obras :: [Obra]
 } deriving Show
-
+{------------------------------------------------------------------------------------------------------------}
 --Modelo de obra
 data Obra = UnaObra{
     nombre_obra :: String,
     fecha_publicacion :: Number
 } deriving Show
-
+{------------------------------------------------------------------------------------------------------------}
 --Declaracion de Obras
 pato1 :: Obra
 pato1 = UnaObra{
@@ -42,12 +43,12 @@ semantivariboficiente = UnaObra{
     fecha_publicacion = 2020
 }
 
-semantica_msm :: Obra
-semantica_msm = UnaObra{
+semanticaMsm :: Obra
+semanticaMsm = UnaObra{
     nombre_obra = "La semántica funcional de Mirtha, Susana y Moria.",
     fecha_publicacion = 2022
 }
-
+{------------------------------------------------------------------------------------------------------------}
 --Declaracion de autores
 robert :: Autor
 robert = UnAutor{
@@ -58,7 +59,7 @@ robert = UnAutor{
 miriam :: Autor
 miriam = UnAutor{
     nombre_autor = "Miriam Lopez",
-    obras = [pato2,semantica_msm]
+    obras = [pato2,semanticaMsm]
 }
 
 tito :: Autor
@@ -66,39 +67,47 @@ tito = UnAutor{
     nombre_autor = "Tito X",
     obras = [semantivariboficiente]
 }
+{------------------------------------------------------------------------------------------------------------}
+-------------------------------------------------Punto 2 (F)--------------------------------------------------
 
---Punto 2 (F)
-
----Plagios
---Punto 3 (R+)
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+{--------------------------------------------------Plagios---------------------------------------------------}
+-------------------------------------------------Punto 3 (R+)-------------------------------------------------
 largoObra :: Obra -> Number
 largoObra = length.nombre_obra
 
 tomarPrimerosNCaracteresObra :: Number -> Obra -> String
-tomarPrimerosNCaracteresObra numero obra = (take numero) (nombre_obra obra)
+tomarPrimerosNCaracteresObra numero obra = take numero (nombre_obra obra)
 
 diferenciaEntreObras :: Obra -> Obra -> Number
 diferenciaEntreObras obra_1 obra_2 = largoObra obra_1 - largoObra obra_2
 
 tomarUltimosNCaracteres :: Number -> Obra -> String
-tomarUltimosNCaracteres numero obra = drop (numero) (nombre_obra obra)
+tomarUltimosNCaracteres numero obra = drop numero (nombre_obra obra)
 
 compararString :: String -> String -> Bool
 compararString str1 str2 = str1 == str2
 
 type Plagio = Obra -> Obra -> Bool
-
+{------------------------------------------------------------------------------------------------------------}
+{----1----}
 copiaLiteral :: Plagio
 copiaLiteral obra_1 obra_2 = compararString (nombre_obra obra_1 ) (nombre_obra obra_2)
-
+{------------------------------------------------------------------------------------------------------------}
+{----2----}
 empiezanIgual :: Number -> Plagio
 empiezanIgual numero obra_1 obra_2 = compararString (tomarPrimerosNCaracteresObra numero obra_1) (tomarPrimerosNCaracteresObra numero obra_2)
-
+{------------------------------------------------------------------------------------------------------------}
+{----3----}
 agregaronIntro :: Plagio
 agregaronIntro obra_1 obra_2 = compararString (tomarUltimosNCaracteres (diferenciaEntreObras obra_1 obra_2) obra_1) (nombre_obra obra_2)
+{------------------------------------------------------------------------------------------------------------}
+{----4----}
 
----Bots
---Punto 4 (ok)
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+{-----------------------------------------------------Bots---------------------------------------------------}
+--------------------------------------------------Punto 4 (ok)------------------------------------------------
 --Modelado de Bots
 data Bot = UnBot {
     nombre_fabricante :: String,
@@ -118,19 +127,19 @@ bot2 = UnBot{
     plagios = [copiaLiteral]
 }
 
---Punto 5 (R+)
+--------------------------------------------------Punto 5 (R+)------------------------------------------------
 --Si el bot detecta plagio
 detectoPlagio :: [Plagio] -> Obra -> Obra -> Bool
 detectoPlagio [x] obra_1 obra_2 = x obra_1 obra_2
 detectoPlagio [] obra_1 obra_2 = False
 detectoPlagio (x:y:xy) obra_1 obra_2 
-    |(x obra_1 obra_2) == False = detectoPlagio (y:xy) obra_1 obra_2
-    |(x obra_1 obra_2) = True
+    |not (x obra_1 obra_2) = detectoPlagio (y:xy) obra_1 obra_2
+    |x obra_1 obra_2 = True
 
 botDetectoPlagio :: Bot -> Obra -> Obra -> Bool
 botDetectoPlagio bot = detectoPlagio (plagios bot)
 
---Punto 6 (R+)
+--------------------------------------------------Punto 6 (R+)------------------------------------------------
 --Cadena de Plagiadores ordenados
 obrasRepetidas :: Bot -> [Obra] -> Obra -> Bool
 obrasRepetidas bot obras obra = any (detectoPlagio (plagios bot) obra) obras
@@ -139,7 +148,7 @@ alMenosUnaListaRepetida :: [Obra] -> [Obra] -> Bot -> Bool
 alMenosUnaListaRepetida [] obras bot = False
 alMenosUnaListaRepetida [x] obras bot = obrasRepetidas bot obras x
 alMenosUnaListaRepetida (x:y:xy) obras bot
-    |obrasRepetidas bot obras x == False = alMenosUnaListaRepetida (y:xy) obras bot
+    |not (obrasRepetidas bot obras x) = alMenosUnaListaRepetida (y:xy) obras bot
     |obrasRepetidas bot obras x = True
 
 todosPlagiadores :: [Autor] -> Bot -> Bool
@@ -147,19 +156,20 @@ todosPlagiadores [] bot = False
 todosPlagiadores [x] bot = False
 todosPlagiadores (x:y:xy) bot = alMenosUnaListaRepetida (obras x) (obras y) bot && todosPlagiadores (y:xy) bot
 
---Punto 7 (F)
+--------------------------------------------------Punto 7 (F)-------------------------------------------------
 --Lista de autores redimidos
 
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
----Infinito
---Punto 8
+{--------------------------------------------------Infinito--------------------------------------------------}
+--------------------------------------------------Punto 8 (ok)------------------------------------------------
 obraInfinita :: Obra
 obraInfinita = UnaObra{
     fecha_publicacion = 0,
     nombre_obra = cycle "Infinito"
 }
 
---Punto 9 (R+)
+--------------------------------------------------Punto 9 (R+)------------------------------------------------
 {-
 --Copia literal
 Si comparo la obra infinita con una obra finita va devolver un booleano, porque recorrerá cada caracter hasta llegar al final de la oracion finita y 
