596b021ae68f13ebe8c478fa6e12edf275139764
Brisa Aylen Brussa <bbrussa@frba.utn.edu.ar>
2024-07-21 17:59:04 -0300

    base

--- src/Library.hs
+++ src/Library.hs
@@ -1,117 +1,101 @@
 module Library where
 import PdePreludat
 import Data.Char (toLower)
+module Library where
+import PdePreludat
+import Data.Char (toLower)
 
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-{----------------------------------------------Autores y Obras-----------------------------------------------} 
--------------------------------------------------Punto 1 (ok)-------------------------------------------------
-{------------------------------------------------------------------------------------------------------------}
+---Autores y Obras
 --Modelo de Autor
 data Autor = UnAutor{
     nombre_autor :: String,
     obras :: [Obra]
 } deriving Show
-{------------------------------------------------------------------------------------------------------------}
+
 --Modelo de obra
 data Obra = UnaObra{
     nombre_obra :: String,
     fecha_publicacion :: Number
 } deriving Show
-{------------------------------------------------------------------------------------------------------------}
+
 --Declaracion de Obras
 pato1 :: Obra
-pato1 = UnaObra "Había una vez un pato." 1997
+pato1 = UnaObra{
+    nombre_obra = "Había una vez un pato.",
+    fecha_publicacion = 1997
+}
 
 pato2 :: Obra
-pato2 = UnaObra "¡Habia una vez un pato!" 1996
+pato2 = UnaObra{
+    nombre_obra = "¡Habia una vez un pato!",
+    fecha_publicacion = 1996
+}
 
 msm :: Obra
-msm = UnaObra "Mirtha, Susana y Moria." 2010
+msm = UnaObra{
+    nombre_obra = "Mirtha, Susana y Moria.",
+    fecha_publicacion = 2010
+}
 
 semantivariboficiente :: Obra
-semantivariboficiente = UnaObra "La semántica funcional del amoblamiento vertebral es riboficiente" 2020
+semantivariboficiente = UnaObra{
+    nombre_obra = "La semántica funcional del amoblamiento vertebral es riboficiente",
+    fecha_publicacion = 2020
+}
 
-semanticaMsm :: Obra
-semanticaMsm = UnaObra "La semántica funcional de Mirtha, Susana y Moria." 2022
-{------------------------------------------------------------------------------------------------------------}
+semantica_msm :: Obra
+semantica_msm = UnaObra{
+    nombre_obra = "La semántica funcional de Mirtha, Susana y Moria.",
+    fecha_publicacion = 2022
+}
+
 --Declaracion de autores
 robert :: Autor
-robert = UnAutor "Robert Tuti" [pato1,msm]
+robert = UnAutor{
+    nombre_autor = "Robert Tuti",
+    obras = [pato1,msm]
+}
 
 miriam :: Autor
-miriam = UnAutor "Miriam Lopez" [pato2,semanticaMsm]
+miriam = UnAutor{
+    nombre_autor = "Miriam Lopez",
+    obras = [pato2,semantica_msm]
+}
 
 tito :: Autor
-tito = UnAutor "Tito X" [semantivariboficiente]
-{------------------------------------------------------------------------------------------------------------}
--------------------------------------------------Punto 2 (F)--------------------------------------------------
-{-
-Conocer la versión cruda de un texto, que consiste en eliminar los acentos de las letras existentes y quitar signos de puntuación y todo carácter que no sea una letra o un número. 
-Por ejemplo, la versión cruda de "Había una vez un pato..." es "Habia una vez un pato"
--}
---Quitar acento de las letras
-quitarAcentoLetra :: Char -> Char
-quitarAcentoLetra 'á' = 'a'
-quitarAcentoLetra 'é' = 'e'
-quitarAcentoLetra 'í' = 'i'
-quitarAcentoLetra 'ó' = 'o'
-quitarAcentoLetra 'ú' = 'u'
-quitarAcentoLetra 'Á' = 'A'
-quitarAcentoLetra 'É' = 'E'
-quitarAcentoLetra 'Í' = 'I'
-quitarAcentoLetra 'Ó' = 'O'
-quitarAcentoLetra 'Ú' = 'U'
-quitarAcentoLetra letra = letra
+tito = UnAutor{
+    nombre_autor = "Tito X",
+    obras = [semantivariboficiente]
+}
 
---Quitar los caracteres especiales de una oracion
-dejarLetrasYNumeros :: String -> String
-dejarLetrasYNumeros = filter (`elem` (['a'..'z']++['A'..'Z']++['0'..'9']++[' ']))
-
---Generar la version cruda de un texto
-reversionarTexto :: String -> String
-reversionarTexto = dejarLetrasYNumeros.map quitarAcentoLetra
-
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-{--------------------------------------------------Plagios---------------------------------------------------}
--------------------------------------------------Punto 3 (R+)-------------------------------------------------
+---Plagios
 largoObra :: Obra -> Number
-largoObra = length.reversionarTexto.nombre_obra
+largoObra = length.nombre_obra
 
 tomarPrimerosNCaracteresObra :: Number -> Obra -> String
-tomarPrimerosNCaracteresObra numero = take numero.reversionarTextoObra
+tomarPrimerosNCaracteresObra numero obra = (take numero) (nombre_obra obra)
 
 diferenciaEntreObras :: Obra -> Obra -> Number
 diferenciaEntreObras obra_1 obra_2 = largoObra obra_1 - largoObra obra_2
 
 tomarUltimosNCaracteres :: Number -> Obra -> String
-tomarUltimosNCaracteres numero = drop numero.reversionarTextoObra
+tomarUltimosNCaracteres numero obra = drop (numero) (nombre_obra obra)
 
 compararString :: String -> String -> Bool
-compararString str1 str2 = reversionarTexto str1 == reversionarTexto str2
-
-reversionarTextoObra :: Obra -> String
-reversionarTextoObra = reversionarTexto.nombre_obra
+compararString str1 str2 = str1 == str2
 
 type Plagio = Obra -> Obra -> Bool
-{------------------------------------------------------------------------------------------------------------}
-{----1----}
-copiaLiteral :: Plagio --obra_1 es copia literal de obra_2
+
+copiaLiteral :: Plagio
 copiaLiteral obra_1 obra_2 = compararString (nombre_obra obra_1 ) (nombre_obra obra_2)
-{------------------------------------------------------------------------------------------------------------}
-{----2----}
-empiezanIgual :: Number -> Plagio --obra_1 comienza igual que obra_2 por x letras
+
+empiezanIgual :: Number -> Plagio
 empiezanIgual numero obra_1 obra_2 = compararString (tomarPrimerosNCaracteresObra numero obra_1) (tomarPrimerosNCaracteresObra numero obra_2)
-{------------------------------------------------------------------------------------------------------------}
-{----3----}
-agregaronIntro :: Plagio --obra_1 agregó intro pero termina igual a obra_2
-agregaronIntro obra_1 obra_2 = compararString (tomarUltimosNCaracteres (diferenciaEntreObras obra_1 obra_2) obra_1) (reversionarTextoObra obra_2)
-{------------------------------------------------------------------------------------------------------------}
-{----4----}
 
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+agregaronIntro :: Plagio
+agregaronIntro obra_1 obra_2 = compararString (tomarUltimosNCaracteres (diferenciaEntreObras obra_1 obra_2) obra_1) (nombre_obra obra_2)
 
-{-----------------------------------------------------Bots---------------------------------------------------}
---------------------------------------------------Punto 4 (ok)------------------------------------------------
+---Bots
 --Modelado de Bots
 data Bot = UnBot {
     nombre_fabricante :: String,
@@ -128,22 +112,20 @@ bot1 = UnBot{
 bot2 :: Bot
 bot2 = UnBot{
     nombre_fabricante = "HP",
-    plagios = [empiezanIgual 21]
+    plagios = [copiaLiteral]
 }
 
---------------------------------------------------Punto 5 (R+)------------------------------------------------
 --Si el bot detecta plagio
 detectoPlagio :: [Plagio] -> Obra -> Obra -> Bool
 detectoPlagio [x] obra_1 obra_2 = x obra_1 obra_2
 detectoPlagio [] obra_1 obra_2 = False
 detectoPlagio (x:y:xy) obra_1 obra_2 
-    |fecha_publicacion obra_1 < fecha_publicacion obra_2 || not (x obra_1 obra_2) = detectoPlagio (y:xy) obra_1 obra_2
-    |x obra_1 obra_2 && fecha_publicacion obra_1 >= fecha_publicacion obra_2 = True
+    |(x obra_1 obra_2) == False = detectoPlagio (y:xy) obra_1 obra_2
+    |(x obra_1 obra_2) = True
 
 botDetectoPlagio :: Bot -> Obra -> Obra -> Bool
 botDetectoPlagio bot = detectoPlagio (plagios bot)
 
---------------------------------------------------Punto 6 (R+)------------------------------------------------
 --Cadena de Plagiadores ordenados
 obrasRepetidas :: Bot -> [Obra] -> Obra -> Bool
 obrasRepetidas bot obras obra = any (detectoPlagio (plagios bot) obra) obras
@@ -152,7 +134,7 @@ alMenosUnaListaRepetida :: [Obra] -> [Obra] -> Bot -> Bool
 alMenosUnaListaRepetida [] obras bot = False
 alMenosUnaListaRepetida [x] obras bot = obrasRepetidas bot obras x
 alMenosUnaListaRepetida (x:y:xy) obras bot
-    |not (obrasRepetidas bot obras x) = alMenosUnaListaRepetida (y:xy) obras bot
+    |obrasRepetidas bot obras x == False = alMenosUnaListaRepetida (y:xy) obras bot
     |obrasRepetidas bot obras x = True
 
 todosPlagiadores :: [Autor] -> Bot -> Bool
@@ -160,25 +142,16 @@ todosPlagiadores [] bot = False
 todosPlagiadores [x] bot = False
 todosPlagiadores (x:y:xy) bot = alMenosUnaListaRepetida (obras x) (obras y) bot && todosPlagiadores (y:xy) bot
 
---------------------------------------------------Punto 7 (F)-------------------------------------------------
-{-
-Dado un conjunto de autores y un bot, encontrar a los autores que  "hicieron plagio pero aprendieron",  
-que significa que luego de que el bot detectara que una de sus obras fue plagio de alguna de los otros autores, 
-nunca más volvió a plagiar. En definitiva, su plagio detectado fue el primero y el último.
--}
 --Lista de autores redimidos
 
----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-{--------------------------------------------------Infinito--------------------------------------------------}
---------------------------------------------------Punto 8 (ok)------------------------------------------------
+---Infinito
 obraInfinita :: Obra
 obraInfinita = UnaObra{
     fecha_publicacion = 0,
     nombre_obra = cycle "Infinito"
 }
 
---------------------------------------------------Punto 9 (R+)------------------------------------------------
 {-
 --Copia literal
 Si comparo la obra infinita con una obra finita va devolver un booleano, porque recorrerá cada caracter hasta llegar al final de la oracion finita y 
