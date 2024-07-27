ed9fb17695ea0fb01b09e2ace52061a3a281a3b4
Brisa Aylen Brussa <bbrussa@frba.utn.edu.ar>
2024-07-26 22:59:39 -0300

    .

--- src/Library.hs
+++ src/Library.hs
@@ -81,6 +81,7 @@ quitarAcento 'Ó' = 'u'
 quitarAcento 'Ú' = 'u'
 quitarAcento letra = letra
 
+--purificarTexto (titulo o_pato_1997)
 purificarTexto :: String -> String
 purificarTexto = quitarCaracteresEspeciales.map quitarAcento
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -108,21 +109,27 @@ cantidadAgregado :: Obra -> Obra -> Number
 cantidadAgregado obra_1 obra_2 = length (purificarTexto (titulo obra_1))-length (purificarTexto (titulo obra_2))
 
 {----1----}
+--obra_1 es copia literal de obra_2
 copiaLiteral :: Plagio
 copiaLiteral obra_1 obra_2 = purificarTexto (titulo obra_1) == purificarTexto (titulo obra_2)
 
 {------------------------------------------------------------------------------------------------------------}
 {----2----}
+--obra_1 empieza igual a obra_2
 empiezaIgual :: Number -> Plagio
-empiezaIgual numero obra_1 obra_2 = take numero (purificarTexto (titulo obra_1)) == purificarTexto (titulo obra_2)
+empiezaIgual numero obra_1 obra_2 = take numero (purificarTexto (titulo obra_1)) == take numero (purificarTexto (titulo obra_2))
 
 {------------------------------------------------------------------------------------------------------------}
 {----3----}
+--obra_1 termina igual a obra_2
 leAgregaronIntro :: Plagio
 leAgregaronIntro obra_1 obra_2 = drop (cantidadAgregado obra_1 obra_2) (purificarTexto (titulo obra_1)) == purificarTexto (titulo obra_2)
 
 {------------------------------------------------------------------------------------------------------------}
 {----4----}
+--Si todas las letras del titulo de la obra1 existen en el titulo de la obra_2
+letrasIguales :: Plagio
+letrasIguales obra_1 obra_2 = all(\letra -> letra `elem` purificarTexto (titulo obra_2)) (purificarTexto (titulo obra_1))
 
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -138,17 +145,19 @@ newtype Bot
 
 --Declaracion de bots
 bot_1 :: Bot
-bot_1 = UnBot [copiaLiteral, leAgregaronIntro]
+bot_1 = UnBot [copiaLiteral, empiezaIgual 10]
 
 bot_2 :: Bot
-bot_2 = UnBot [empiezaIgual 21]
+bot_2 = UnBot [leAgregaronIntro]
 
 --------------------------------------------------Punto 5 (R+)------------------------------------------------
 {-Un bot detecta si una obra es plagio de otra si verifica alguna de las formas de detección que maneja.-}
 {------------------------------------------------------------------------------------------------------------}
+--Obra1 es plagio de Obra2 si el plagio dio positivo y Obra1 salió despues de Obra2
 aplicarPlagio :: Obra -> Obra -> Plagio ->  Bool
 aplicarPlagio obra_1 obra_2 plagio = plagio obra_1 obra_2 && (año_publicacion obra_1 > año_publicacion obra_2)
 
+--El bot detecta si Obra1 es plagio de Obra2
 botDetectarPlagio :: Bot -> Obra -> Obra -> Bool
 botDetectarPlagio bot obra_1 obra_2 = any (aplicarPlagio obra_1 obra_2) (formasDeteccion bot)
 
@@ -157,23 +166,40 @@ botDetectarPlagio bot obra_1 obra_2 = any (aplicarPlagio obra_1 obra_2) (formasD
 plagió al primero, el tercero al segundo, y así. Se considera que un autor plagió a otro cuando 
 alguna de sus obras es plagio de alguna de las del otro según el bot.-}
 {------------------------------------------------------------------------------------------------------------}
+--El bot detecta si la obra ingresada es plagio del autor ingresado
 autorPlagio :: Autor -> Bot -> Obra -> Bool
 autorPlagio autor bot obra = any (botDetectarPlagio bot obra) (obras autor)
 
-autorSPlagioAutorO :: Autor -> Autor -> Bot -> Bool
-autorSPlagioAutorO autor_s autor_o bot = any (autorPlagio autor_s bot) (obras autor_o)
+--El bot detecta si el autorS hizo plagio al autorO
+autorSPlagioAutorO :: Autor -> Bot -> Autor -> Bool
+autorSPlagioAutorO autor_s bot autor_o = any (autorPlagio autor_o bot) (obras autor_s)
 
+--El bot detecta si es una cadena de plagiadores
 esCadenaDePlagiadores :: [Autor] -> Bot -> Bool
 esCadenaDePlagiadores [] bot = False
-esCadenaDePlagiadores [x,y] bot = autorSPlagioAutorO x y bot
-esCadenaDePlagiadores (x:y:xy) bot = autorSPlagioAutorO x y bot && esCadenaDePlagiadores xy bot
+esCadenaDePlagiadores [x,y] bot = autorSPlagioAutorO x bot y 
+esCadenaDePlagiadores (x:y:xy) bot = autorSPlagioAutorO x bot y  && esCadenaDePlagiadores xy bot
 
 --------------------------------------------------Punto 7 (F)-------------------------------------------------
 {-Dado un conjunto de autores y un bot, encontrar a los autores que  "hicieron plagio pero aprendieron",  
 que significa que luego de que el bot detectara que una de sus obras fue plagio de alguna de los otros 
 autores, nunca más volvió a plagiar. En definitiva, su plagio detectado fue el primero y el último.-}
 {------------------------------------------------------------------------------------------------------------}
+--Si la obra es plagio de algun autor de la lista
+obraPlagioDeAlgunAutor :: [Autor] -> Bot -> Obra -> Bool
+obraPlagioDeAlgunAutor autores bot obra = any(\autor -> autorPlagio autor bot obra) autores
 
+--Lista de obras plagio segun la lista de autores a comparar
+obrasPlagioDelAutor :: Autor -> [Autor] -> Bot -> [Obra]
+obrasPlagioDelAutor autor autores bot = filter(obraPlagioDeAlgunAutor autores bot) (obras autor)
+
+--Si solo plagió una vez y nunca más es que ya aprendió
+autorYaNoPlagia :: Autor -> [Autor] -> Bot -> Bool
+autorYaNoPlagia autor autores bot = length(obrasPlagioDelAutor autor autores bot) == 1
+
+--Lista de autores que plagiaron pero aprendieron
+autoresQueYaNoPlagian :: [Autor] -> Bot -> [Autor]
+autoresQueYaNoPlagian autores bot = filter (\autor -> autorYaNoPlagia autor autores bot) autores
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -182,8 +208,34 @@ autores, nunca más volvió a plagiar. En definitiva, su plagio detectado fue el
 {-Codificar una obra infinita. ¿Qué sucede si se desea verificar si esa obra es plagio de otra con cada 
 una de las formas existentes? Justificar conceptualmente en cada caso.-}
 {------------------------------------------------------------------------------------------------------------}
---Obra infinita
+--------------Obra infinita
 obraInfinita :: Obra
 obraInfinita = UnaObra (cycle "Obra infinita... ") 2000
+
+obraInfinita2 :: Obra
+obraInfinita2 = UnaObra (cycle "X ") 2000
 {------------------------------------------------------------------------------------------------------------}
---Respuestas
+--------------Respuestas
+--copiaLiteral
+{-En este caso existen tres posibles caso. 
+Si se compara la obra infinita con una finita, gracias al Lazy Evaluation, una vez se terminan los elementos comparables de una
+de las dos cadenas, queda claro que no son iguales y da False.
+Si se comparan dos obras infinitas distintas, El Lazy Evaluation al encontrar tan solo un elemento distinto, cortara el proceso y dará False sin importar si existen aun elementos
+sin comparar.
+Si se comparan dos listas infinitas iguales, jamás dará resultado y no habrá Lazy Evaluation, ya que no podrá jamás dejar de comparar elementos.-}
+
+--empiezaIgual
+{-Gracias al Lazy Evaluation solo toma una cantidad finita de letras sin tomar en cuenta el largo de las cadenas, 
+por lo que dará un resultado sin problema el cual puede ser verdadero o falso.-}
+
+--leAgregaronIntro
+{-En este caso no existe Lazy Evaluation ya que no puede calcular el largo de la obra infinita, por lo que no existe resultado posible.-}
+
+--letrasIguales
+{-Aquí pueden ocurrir cuatro cosas.
+Si realizo letrasIguales obraInfinita o_pato_1996, dará falso, porque gracias al Lazy Evaluation, al encontrar una sola letra de la obra infinita que no esté en
+la segunda obra, dará el resultado ya que no será posible que todas las letras se encuentren.
+Si comparo cada letra de la obraInfinita con una obra que tenga el mismo texto que se reíte en ciclo, jamás podrá dar falso, ya que todas las letras existen dentro de la cadena finita
+y no podrá dejar de comparar. No hay lazy evaluation.
+Si comparo dos obras infinitas, no importa si son distintas, porque buscara infinitamente de todas formas, impidiendo dar un resultado. No hay lazy evaluation.
+Si realizo letrasIguales o_pato_1996 obraInfinita, jamás dará resultado, porque buscará de forma infinita las letras del titulo finito dentro del infinito. No hay lazy evaluation.-}
